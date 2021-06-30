ecd2490e28409758503699f72885c77e73666972
Boris <toborto@yandex.ru>
2021-06-30 19:32:41 +0300

    webpack4 -> webpack5

--- src/main/javascript/lyra/webpack.config.js
+++ src/main/javascript/lyra/webpack.config.js
@@ -3,44 +3,44 @@ const DojoWebpackPlugin = require('dojo-webpack-plugin');
 const loaderConfig = require('./config/loaderConfig');
 
 module.exports = {
-    mode: 'development',
+  mode: 'development',
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ],
-            }, {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'url-loader'
-            },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
         ],
-    },
-
-    plugins: [
-        new VueLoaderPlugin(),
-        new DojoWebpackPlugin({
-            loaderConfig,
-            environment: {dojoRoot: '.'}, // used at run time for non-packed resources (e.g. blank.gif)
-            buildEnvironment: {dojoRoot: 'node_modules'}, // used at build time
-            locales: ['en', 'ru'],
-        }),
+      }, {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url-loader',
+      },
     ],
+  },
 
-    optimization: {
-        splitChunks: false,
-    },
+  plugins: [
+    new VueLoaderPlugin(),
+    new DojoWebpackPlugin({
+      loaderConfig,
+      environment: { dojoRoot: '.' }, // used at run time for non-packed resources (e.g. blank.gif)
+      buildEnvironment: { dojoRoot: 'node_modules' }, // used at build time
+      locales: ['en', 'ru'],
+    }),
+  ],
+
+  optimization: {
+    splitChunks: false,
+  },
 
 };
